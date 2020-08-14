7e1a98660fc2410393d34feff00f5dbba6323545
phette23 <phette23@gmail.com>
2020-08-14 11:44:44 -0700

    forgot one last plugin, auth_db

--- mk-plugin-cfg-files.sh
+++ mk-plugin-cfg-files.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-PLUGINS=("auth_manual" "enrol_manual" "enrol_database" "enrol_meta" "enrol_self" "enrol_guest" "enrol_cohort" "auth_cas" "block_attendance" "block_point_view" "format_grid" "local_course_template" "mod_attendance" "mod_zoom" "report_customsql")
+PLUGINS=("auth_db" "auth_manual" "enrol_manual" "enrol_database" "enrol_meta" "enrol_self" "enrol_guest" "enrol_cohort" "auth_cas" "block_attendance" "block_point_view" "format_grid" "local_course_template" "mod_attendance" "mod_zoom" "report_customsql")
 
 for PLUGIN in ${PLUGINS[@]}; do
     sudo php /opt/moodle38/admin/cli/cfg.php --component=${PLUGIN} --json > ~/${PLUGIN}.json
