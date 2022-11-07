60d555831c70e576333670628fc28c299a566b15
phette23 <phette23@gmail.com>
2022-11-07 13:35:59 -0800

    shellcheck for get-all-cfgs.sh, ref #1

--- get-all-cfgs.sh
+++ get-all-cfgs.sh
@@ -8,8 +8,8 @@ fi
 
 # setup tasks
 mkdir -p data
-MOODLE_DIR=/bitnami/moodle
-POD=$(kubectl get pods -n ${NS} | grep 'moodle-' | cut -d ' ' -f 1)
+MOODLE_DIR="/bitnami/moodle"
+POD=$(kubectl get pods -n "${NS}" | grep 'moodle-' | cut -d ' ' -f 1)
 
 if [[ -z ${POD} ]]; then
     echo "Unable to find a Moodle app pod in namespace ${NS}, are you sure that's right?" >&2
@@ -20,17 +20,20 @@ fi
 # nicely formatted JSON files thanks to jq
 get_config () {
     echo "Getting configuration for $1 on Moodle pod ${POD} in namespace ${NS}"
-    kubectl exec -n ${NS} ${POD} -it -- php ${MOODLE_DIR}/admin/cli/cfg.php --component=$1 --json | jq > data/$1.json
+    kubectl exec -n "${NS}" "${POD}" -t -- php "${MOODLE_DIR}/admin/cli/cfg.php" --component="$1" --json | jq > data/"$1.json"
 }
 
 get_config 'core'
 
 # full list of plugins with their own settings
 # `moosh sql-run`` produces a bunch of garbage output so we have to strip it out
-kubectl exec -n ${NS} ${POD} -it -- moosh -n --moodle-path=${MOODLE_DIR} sql-run 'SELECT DISTINCT plugin FROM {config_plugins}' | grep -o '=> .*' | tr -d '=> \r' > plugins.txt
+kubectl exec -n "${NS}" "${POD}" -- moosh -n --moodle-path="${MOODLE_DIR}" sql-run 'SELECT DISTINCT plugin FROM {config_plugins}' | grep -o '=> .*' | tr -d '=> \r' > plugins.txt
 
-for PLUGIN in $(cat plugins.txt); do
-    get_config ${PLUGIN}
+# https://www.shellcheck.net/wiki/SC2013
+# plugin names should not have spaces, but just in case
+grep -v '^ *#' < plugins.txt | while IFS= read -r PLUGIN
+do
+    get_config "${PLUGIN}"
 done
 
 rm plugins.txt
